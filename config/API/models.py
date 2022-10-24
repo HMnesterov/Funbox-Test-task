7210ba4b9baea8e148b01899bf08a768e16163b9
BenitoSwaggolini <profie_player@mail.ru>
2022-10-24 20:53:59 +0500

    Docker

--- config/API/models.py
+++ config/API/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 class Link(models.Model):
     link = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.link
+
+    def __eq__(self, obj):
+        return self.link == obj.link
